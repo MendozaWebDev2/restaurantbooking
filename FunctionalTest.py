32ea139da26d8f724934ee627b61177036b2ec4c
eden <eden.mendoza@gsfe.tupcavite.edu.ph>
2021-04-04 11:43:22 +0800

    add

--- FunctionalTest.py
+++ FunctionalTest.py
@@ -14,23 +14,34 @@ class PageTest(unittest.TestCase):
 	def test_browser_test(self):
 		'''browser.get('http://127.0.0.1:8000')'''
 
-	def test_start_list_and_retrieve_it(self): 
+	def test_browser_title(self): 
 		self.browser.get('http://localhost:8000')
-		'''self.assertIn('WTG in Dasmarinas', self.browser.title)'''
+		self.assertIn('Add Place', self.browser.title)
+
 		headerText = self.browser.find_element_by_tag_name('h1').text
-		'''self.assertIn('Group', headerText)'''
-		'''inputbox = self.browser.find_element_by_id('idNewEntry')
-		self.assertEqual(inputbox.get_attribute('placeholder'), 'Group name you have seen')
-		inputbox.send_keys('Twice')
-		inputbox.send_keys(Keys.ENTER)
+		self.assertIn('Add Place', headerText)
+
+		input1= self.browser.find_element_by_id('in1').text
+		self.assertIn('Name', input1)
+		name = self.browser.find_element_by_id('Name').send_keys("Eden Mendoza")
+
+		input2= self.browser.find_element_by_id('in2').text
+		self.assertIn('Name of Suggested Restaurant or Site', input2)
+		name = self.browser.find_element_by_id('Name1').send_keys("Batang 9Teas-Milk Tea")
+
+		input3= self.browser.find_element_by_id('in3').text
+		self.assertIn('Address of Suggested Restaurant or Site', input3)
+		name = self.browser.find_element_by_id('Address').send_keys("Brgy. San Esteban, Dasmarinas")
+
+
+		'''inputbox.send_keys('eden')
+		inputbox.send_keys('Keys.ENTER')
 		time.sleep(1)
 		table = self.browser.find_element_by_id('idListTable')
 		rows = table.find_element_by_tag_name('tr')
-		self.assertTrue(any(row.text == '1:Twice'))'''
+		self.assertTrue(any(row.text == '1:Twice'))'''	
 
 
 
 if __name__== '__main__':
 	unittest.main(warnings='ignore')
-
-
