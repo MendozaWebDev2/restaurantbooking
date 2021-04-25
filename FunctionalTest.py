996004e012f558a852a756151f46575f0427e010
eden <eden.mendoza@gsfe.tupcavite.edu.ph>
2021-04-26 05:18:51 +0800

    add

--- FunctionalTest.py
+++ FunctionalTest.py
@@ -1,42 +1,96 @@
 from selenium import webdriver
-browser = webdriver.Firefox()
-browser.get('http://127.0.0.1:8000')
+import unittest
 
-'''class PageTest(unittest.TestCase):
+from selenium.webdriver.common.keys import Keys
+import time
 
+class PageTest(unittest.TestCase):
+
+	
 	def setUp(self):
 		self.browser = webdriver.Firefox()
+		
 
-	def test_browser_test(self):
-		browser.get('http://127.0.0.1:8000')
-
-	def test_browser_title(self): 
+	def test_browser_title(self):
 		self.browser.get('http://localhost:8000')
 		self.assertIn('Add Suggested Place', self.browser.title)
 
-		headerText = self.browser.find_element_by_tag_name('h1').text
-		self.assertIn('Add Suggested Place', headerText)
+		denText = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('Add Suggested Place', denText)
 
-		input1= self.browser.find_element_by_id('in1').text
-		self.assertIn('Name', input1)
-		name = self.browser.find_element_by_id('Name').send_keys("Eden Mendoza")
-		time.sleep(2)
+		p1 = self.browser.find_element_by_tag_name('p').text
+		self.assertIn('Name', p1)
 
-		input2= self.browser.find_element_by_id('in2').text
-		self.assertIn('Name of Suggested Restaurant or Site', input2)
-		name = self.browser.find_element_by_id('Name1').send_keys("Batang 9Teas-Milk Tea")
-		time.sleep(2)
+		p2 = self.browser.find_element_by_id('in2').text
+		self.assertIn('Name of Suggested Restaurant or Site', p2)
 
-		input3= self.browser.find_element_by_id('in3').text
-		self.assertIn('Address of Suggested Restaurant or Site', input3)
-		name = self.browser.find_element_by_id('Address').send_keys("Brgy. San Esteban, Dasmarinas")
-		time.sleep(2)
+		p3 = self.browser.find_element_by_id('in3').text
+		self.assertIn('Address of Suggested Restaurant or Site', p3)
 
-		input4= self.browser.find_element_by_id('in4').text
-		self.assertIn('Date', input)
-		name = self.browser.find_element_by_id('Date').send_keys("24/04/2021")
-		time.sleep(2)
+		p4 = self.browser.find_element_by_id('in4').text
+		self.assertIn('Date', p4)
+
+		form = self.browser.find_element_by_tag_name('form')
+
+		input1 = self.browser.find_element_by_id('Name')  
+		self.assertEqual(input1.get_attribute('placeholder'),'Fisrtname, Lastname')
+		input1 = self.browser.find_element_by_id('Name').send_keys("Eden Mendoza")
+		time.sleep(1)
+
+		input2 = self.browser.find_element_by_id('Name1')  
+		self.assertEqual(input2.get_attribute('placeholder'),'Type Here')
+		input2 = self.browser.find_element_by_id('Name1').send_keys("Unli Wings")
+		time.sleep(1)
+
+		input3 = self.browser.find_element_by_id('Address')  
+		self.assertEqual(input3.get_attribute('placeholder'),'Type Here')
+		input3 = self.browser.find_element_by_id('Address').send_keys("Palipran 2, Dasmarinas City, Cavite")
+		time.sleep(1)
+
+		date = self.browser.find_element_by_id('Date').click()
+		time.sleep(1)
+		date = self.browser.find_element_by_id('Date').send_keys("2021-04-26")
+		time.sleep(1)
+
+		submit = self.browser.find_element_by_id('Submit').click()
+		time.sleep(3)
+
+		self.browser.quit()	
+		self.browser = webdriver.Firefox()
+		self.browser.get('http://localhost:8000')
+
+		input1 = self.browser.find_element_by_id('Name')  
+		self.assertEqual(input1.get_attribute('placeholder'),'Fisrtname, Lastname')
+		input1 = self.browser.find_element_by_id('Name').send_keys("Charles DelaCruz")
+		time.sleep(1)
+
+		input2 = self.browser.find_element_by_id('Name1')  
+		self.assertEqual(input2.get_attribute('placeholder'),'Type Here')
+		input2 = self.browser.find_element_by_id('Name1').send_keys("Samgyupsal")
+		time.sleep(1)
+
+		input3 = self.browser.find_element_by_id('Address')  
+		self.assertEqual(input3.get_attribute('placeholder'),'Type Here')
+		input3 = self.browser.find_element_by_id('Address').send_keys("Salawag, Dasmarinas City, Cavite")
+		time.sleep(1)
+
+		date = self.browser.find_element_by_id('Date').click()
+		time.sleep(1)
+		date = self.browser.find_element_by_id('Date').send_keys("2021-04-27")
+		time.sleep(1)
+
+		submit = self.browser.find_element_by_id('Submit').click()
+		time.sleep(3)
+		self.browser.quit()
 
 
-if __name__== '__main__':
-	unittest.main(warnings='ignore')'''
+
+
+
+
+
+		
+
+
+if __name__ == '__main__':
+	unittest.main()
